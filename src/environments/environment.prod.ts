cd2d1c2d3faa312993998b4038854aaa5e2e9fe0
santiortizutn <santyno09@gmail.com>
2021-03-26 01:32:52 -0300

    para heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCgsvLDOIIs2fSPxkqGya0jpjS8FGguhtM",
+    authDomain: "saladejuegos-94f77.firebaseapp.com",
+    projectId: "saladejuegos-94f77",
+    storageBucket: "saladejuegos-94f77.appspot.com",
+    messagingSenderId: "726113077594",
+    appId: "1:726113077594:web:e168be5bc0f35cf28ecb63",
+    measurementId: "G-0N7PKLRVY0"
+  },
+
+  hostFirebase:'https://saladejuegos-94f77-default-rtdb.firebaseio.com/'
 };
